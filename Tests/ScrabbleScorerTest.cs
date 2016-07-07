2f22929675f58b59a029472b0d7ef46e2e71b21b
Adam Luchini <aluchini@gmail.com>
2016-07-07 08:17:50 -0700

    add project folders, project.json, dictionary properties

---
+++ Tests/ScrabbleScorerTest.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace ScrabbleScorer.Objects
+{
+  public class ScrabbleScorerTest
+  {
+    public static int WordScore(string input)
+    {
+      Dictionary<char, int> newDictionary = new Dictionary<char, int> {};
+      
+    }
+  }
+}
